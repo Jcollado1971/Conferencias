311a98be253e69a4f036c4c0162bef5ce873a53b
jcollado <josecollado1971@hotmail.com>
2021-10-06 20:36:01 +0200

    paso 9

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -48,6 +48,15 @@ class Comment
      */
     private $photoFilename;
 
+
+
+    public function __toString(): string
+   {
+
+       return (string) $this->getEmail();
+
+   }
+
     public function getId(): ?int
     {
         return $this->id;
