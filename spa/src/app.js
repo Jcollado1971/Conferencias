4d66bf704bc695c23de044561ca427b46e975359
Jcollado <josecollado1971@hotmail.com>
2021-12-15 17:36:41 +0100

    paso 28 realizado, excepto pruebas

--- spa/src/app.js
+++ spa/src/app.js
@@ -2,12 +2,54 @@
 
 
 
-import { h, render, Component } from 'preact';
+import {h, render} from 'preact';
+import '../assets/styles/app.scss';
 
-class App extends Component {
-  render() {
-    return <h1>Hello, world!</h1>;
-  }
+import {h, render} from 'preact';
+import {Router, Link} from 'preact-router';
+import {useState, useEffect} from 'preact/hooks';
+
+import {findConferences} from './api/api';
+import Home from './pages/home';
+import Conference from './pages/conference';
+
+function App() {
+    const [conferences, setConferences] = useState(null);
+
+    useEffect(() => {
+        findConferences().then((conferences) => setConferences(conferences));
+    }, []);
+
+    if (conferences === null) {
+        return <div className="text-center pt-5">Loading...</div>;
+    }
+
+    return (
+        <div>
+            <header className="header">
+                <nav className="navbar navbar-light bg-light">
+                    <div className="container">
+                        <Link className="navbar-brand mr-4 pr-2" href="/">
+                            &#128217; Guestbook
+                        </Link>
+                    </div>
+                </nav>
+
+                <nav className="bg-light border-bottom text-center">
+                    {conferences.map((conference) => (
+                        <Link className="nav-conference" href={'/conference/'+conference.slug}>
+                            {conference.city} {conference.year}
+                        </Link>
+                    ))}
+                </nav>
+            </header>
+
+            <Router>
+                <Home path="/" conferences={conferences} />
+                <Conference path="/conference/:slug" conferences={conferences} />
+            </Router>
+        </div>
+    )
 }
 
-render(<App />, document.body);
+render(<App />, document.getElementById('app'));
